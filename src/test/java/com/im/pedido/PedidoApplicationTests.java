cc5e429bc4ebd7547ca9d051d3da4e837bdc3dab
Igor Martins <dev.igor.martins@gmail.com>
2021-10-27 16:40:07 -0300

    Criação do Projeto API de Pedidos

---
+++ src/test/java/com/im/pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.im.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
